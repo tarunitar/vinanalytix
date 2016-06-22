3ce615ac5860b173f4da1c43e1639dc8a49fcdbd
Tarunraj Amuthan <tarun1995@gmail.com>
2016-06-22 16:53:51 -0700

    added main.ts and app.component

---
+++ main.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from 'angular2/platform/browser'
+import {ApComponent} from './app.component'
+import {ROUTER_PROVIDERS} from 'angular2/router'
+
+bootstrap(AppComponent, [ROUTER_PROVIDERS]);
